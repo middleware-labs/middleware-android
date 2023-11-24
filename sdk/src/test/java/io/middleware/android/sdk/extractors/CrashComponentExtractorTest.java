3111274917fcabf2627786b1b84e180b935d10a6
Archish <archish@middleware.io>
2023-11-24 18:32:24 +0530

    :white_check_mark: test fixes for event attribute

--- sdk/src/test/java/io/middleware/android/sdk/extractors/CrashComponentExtractorTest.java
+++ sdk/src/test/java/io/middleware/android/sdk/extractors/CrashComponentExtractorTest.java
@@ -3,6 +3,7 @@ package io.middleware.android.sdk.extractors;
 
 import static io.middleware.android.sdk.utils.Constants.COMPONENT_CRASH;
 import static io.middleware.android.sdk.utils.Constants.COMPONENT_KEY;
+import static io.middleware.android.sdk.utils.Constants.EVENT_TYPE;
 import static io.opentelemetry.sdk.testing.assertj.OpenTelemetryAssertions.assertThat;
 
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,11 @@ public class CrashComponentExtractorTest {
         AttributesBuilder attributesBuilder = Attributes.builder();
         crashComponentExtractor.onStart(attributesBuilder, null, null);
         assertThat(attributesBuilder.build())
-                .hasSize(1)
+                .hasSize(2)
                 .containsEntry(COMPONENT_KEY, COMPONENT_CRASH);
+        assertThat(attributesBuilder.build())
+                .hasSize(2)
+                .containsEntry(EVENT_TYPE, COMPONENT_CRASH);
     }
 
 }
